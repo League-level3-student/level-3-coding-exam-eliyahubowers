5e476e55e89282a899bbee79338e030cb1d9c4d7
League Student <league@WTS14-4.attlocal.net>
2019-06-12 16:14:31 -0700

    level3 exam

--- src/HashMapCalculator.java
+++ src/HashMapCalculator.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class HashMapCalculator {
 	int commonKeyValuePairs(HashMap<String,String> hashOne, HashMap<String,String> hashTwo) {
 		int matching = 0;
+		ArrayList<String> keys = new ArrayList<String>();
+		keys.addAll(0, hashOne.keySet());
 		for(int i = 0; i < hashOne.size(); i++) {
-			for(int j = 0; j < hashTwo.size(); j++) {
-				if(hashOne.equals(hashTwo)) {
-					matching ++;
-				}
+			String value1 = hashOne.get(keys.get(i));
+			String value2 = hashTwo.get(keys.get(i));
+			if(value1.equals(value2)) {
+				matching ++;
 			}
 		}
 		return matching;
